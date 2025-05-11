62e0616d213cb8500c300f6ca177a7f83f77d1bc
Sudil Paudel <sudilpaudel@Sudils-MacBook-Pro.local>
2025-05-11 16:24:59 +0545

    fixed issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Register from './pages/register';
+
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoutes';
